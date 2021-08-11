e53f4cd7fc9c0dbbd8485c06172b1aa30c09e7d0
Helio Tejedor <heltena@northwestern.edu>
2021-08-11 09:35:13 -0500

    First commit

---
+++ GetComponents/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  GetComponents
+//
+//  Created by Helio Tejedor on 8/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var selectedColor: Color = .clear
+    @State var r: CGFloat = 0.0
+    @State var g: CGFloat = 0.0
+    @State var b: CGFloat = 0.0
+    @State var a: CGFloat = 0.0
+    
+    func refresh(color: Color) {
+        UIColor(color).getRed(&r, green: &g, blue: &b, alpha: &a)
+        print("\(r), \(g), \(b), \(a)")
+    }
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            Text("\(r)")
+            Text("\(g)")
+            Text("\(b)")
+            Text("\(a)")
+            HStack {
+                Button("Red") { refresh(color: .red) }
+                Button("Accent") { refresh(color: .accentColor) }
+                Button("Green") { refresh(color: Color("ExampleColor")) }
+            }
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GetComponents/GetComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetComponentsApp.swift
+//  GetComponents
+//
+//  Created by Helio Tejedor on 8/11/21.
+//
+
+import SwiftUI
+
+@main
+struct GetComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
